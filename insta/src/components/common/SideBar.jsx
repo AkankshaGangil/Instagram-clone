9692bf5c9913173d1c406137bc45d8dd5cb5551e
Akanksha Gangil <123535273+AkankshaGangil@users.noreply.github.com>
2023-11-23 12:25:54 +0530

    Update SideBar.jsx

--- insta/src/components/common/SideBar.jsx
+++ insta/src/components/common/SideBar.jsx
@@ -7,21 +7,22 @@ import VideoLibraryOutlinedIcon from '@mui/icons-material/VideoLibraryOutlined';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
+import StorageIcon from '@mui/icons-material/Storage';
 
 const SideBar = () => {
     return(
-        <div className="sidebarcontainer">
+   <div className="sidebarcontainer">
             <ul>
                 <li><img width="130px"   src="https://www.edigitalagency.com.au/wp-content/uploads/instagram-logo-white-text-black-background.png" alt=""/></li>
                 
-                <li><span > <HomeOutlinedIcon/></span>Home</li>
-                <li><span><SearchIcon/></span>Search</li>
-                <li><span><ExploreOutlinedIcon/></span>Explore</li>
-                <li><span><VideoLibraryOutlinedIcon/></span>Reels</li>
-                <li><span><ChatOutlinedIcon /></span>Messages</li>
-                <li><span><FavoriteBorderIcon/></span>Notification</li>
-                <li><span><AddCircleOutlineIcon/></span>Create</li>
-                <li><span></span>Profile</li>
+                <button className="side_button"><HomeOutlinedIcon/><span > Home</span></button>
+                <button className="side_button"><SearchIcon/><span>Search</span></button>
+                <button className="side_button"><ExploreOutlinedIcon/><span>Explore</span></button>
+                <button className="side_button"><VideoLibraryOutlinedIcon/><span>Reels</span></button>
+                <button className="side_button"><ChatOutlinedIcon /><span>Messages</span></button>
+                <button className="side_button"><FavoriteBorderIcon/><span>Notification</span></button>
+                <button className="side_button"><AddCircleOutlineIcon/><span>Create</span></button>
+                <button className="side_button" style={{position:"fixed", bottom:"3px"}}><StorageIcon/><span>More</span></button>
             </ul>
         </div>
     )
